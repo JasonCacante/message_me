263de25374dc043dde2c19f08e55ca533ba90044
JasonCacante <jason.cacante@gmail.com>
2023-06-15 15:11:16 -0500

    feat(session_controller.rb): add login features as create session and destroy session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,33 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
+  before_action :logged_in_redirect, only: %i[new create]
+  def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user&.authenticate(params[:session][:password]) # if user exists and password is correct
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in.'
+      redirect_to root_path
+    else
+      flash[:error] = 'There was something wrong with your login information.'
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have successfully logged out.'
+    redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    return unless logged_in?
+
+    flash[:error] = 'You are already logged in.'
+    redirect_to root_path
   end
 end
