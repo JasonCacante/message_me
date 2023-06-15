dd32f9f38f03ec2fdc4a5eb49ccb823b2cbd4053
JasonCacante <jason.cacante@gmail.com>
2023-06-15 15:20:39 -0500

    feat(routes.rb): add routes for session and messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "chatroom#index"
+  root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
 end
