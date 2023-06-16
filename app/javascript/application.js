91892f016efefde33e5dddea6661365249d1afc7
JasonCacante <jason.cacante@gmail.com>
2023-06-15 21:42:15 -0500

    feat(application.js): add two functions to controller the scrolling of the page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,9 +6,26 @@ import "semantic-ui"
 import "./channels"
 import "@rails/actioncable" 
 
+function scrollBottom () {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+function submitMessage(event) {
+  $('#message_body').on('keydown', function (e) {
+    if (e.key == 'Enter') {
+      $('button').click();
+      e.target.value = "";
+    }
+  });
+}
+
 $(document).on('turbo:load', function () {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  submitMessage();
+  scrollBottom();
 });
