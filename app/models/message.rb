578f44be5d759b756a6964521e9c3450c95b7ed7
JasonCacante <jason.cacante@gmail.com>
2023-06-15 21:43:41 -0500

    feat(message.rb): add scope to limit the number of message is fetched to the client

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length: { minimum: 2, maximum: 1000 }
+
+  scope :custom_display, -> { order(:created_at).last(20) }
 end
