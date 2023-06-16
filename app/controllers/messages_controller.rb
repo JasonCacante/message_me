aaa93bdabe58ee19d340fffdcaba958d265e830d
JasonCacante <jason.cacante@gmail.com>
2023-06-15 21:41:27 -0500

    feat(messages_controller.rb): use a partial to render the messages and given then an specific format

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,17 +5,18 @@ class MessagesController < ApplicationController
 
   def create
     message = current_user.messages.build(message_params)
-    if message.save
-      ActionCable.server.broadcast('chatroom_channel', { message: message.body })
-    else
-      flash[:error] = 'Message was not saved.'
-    end
-    redirect_to root_path
+    return unless message.save
+
+    ActionCable.server.broadcast('chatroom_channel', { message: message_render(message) })
   end
 
   private
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: { message: })
   end
 end
